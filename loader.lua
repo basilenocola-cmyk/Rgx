1bab8f5bc2b71ff57b1368a6f4e99f77babe6982
basilenocola-cmyk <basilenocola@gmail.com>
2026-01-13 16:20:29 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,12 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/basilenocola-cmyk/Rgx/main/loader.lua"))()
+print("LOADER AVVIATO")  -- Checkpoint iniziale
+
+local ok, err = pcall(function()
+    print("Provo a caricare hub.lua")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/basilenocola-cmyk/Rgx/main/hub.lua"))()
+end)
+
+if ok then
+    print("HUB CARICATO CORRETTAMENTE")
+else
+    warn("ERRORE NEL CARICARE HUB:", err)
+end
